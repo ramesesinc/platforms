8daf54101f6da51025581b8f396d78e62b60ec42
worgflores <worgflores@gmail.com>
2014-05-09 14:25:59 +0800

    added template

---
+++ template/core_template.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enableDelayedExpansion
+if exist platform.conf (
+	for /f "delims=" %%x in (platform.conf) do (
+		set str=%%x
+		if not "!str:~0,1!" == "#" set "%%x" 
+	) 
+) 
+
+set JAVA=java
+if not "%JAVA_HOME%" == "" set JAVA=%JAVA_HOME%\bin\java
+
+echo.
+echo.========================================================================
+echo.
+echo   Platform    : Osiris3 Client Platform 
+echo   Application : ETRACS Version 2.5
+echo.
+echo   JAVA_HOME   : %JAVA_HOME%
+echo   JAVA_OPTS   : %JAVA_OPTS%
+echo   JAVA        : %JAVA%
+echo.
+echo.========================================================================
+echo.
+
+"%JAVA%" %JAVA_OPTS% -cp lib/*;. com.rameses.osiris3.platform.OSBootStrap
+endlocal
