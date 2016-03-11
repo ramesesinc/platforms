4305e5968a8e500868296cc4026e9d112d93d0f7
worgflores <worgflores@gmail.com>
2016-03-11 10:21:13 +0800

    added julies_platform

---
+++ julies_platform/julies_platform.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enableDelayedExpansion
+if exist platform.conf (
+	for /f "delims=" %%x in (platform.conf) do (
+		set str=%%x
+		if not "!str:~0,1!" == "#" set "%%x" 
+	) 
+) 
+
+set JAVA=java
+if not "%JAVA_HOME%" == "" set JAVA=%JAVA_HOME%\bin\java
+
+echo.
+echo.========================================================================
+echo.
+echo   Platform    : Osiris3 Client Platform 
+echo   Application : Julies Application 
+echo.
+echo   JAVA_HOME   : %JAVA_HOME%
+echo   JAVA_OPTS   : %JAVA_OPTS%
+echo   JAVA        : %JAVA%
+echo.
+echo.========================================================================
+echo.
+
+"%JAVA%" %JAVA_OPTS% -cp lib/ext/%PROCESSOR_ARCHITECTURE%;lib/*;. com.rameses.osiris3.platform.OSBootStrap
+endlocal
+rem pause
